0587d9c993ff848e471a4e1873bdda729d65f7e8
Song Jae Hyeon <0nocsace@gmail.com>
2021-09-15 13:55:41 +0900

    blades

---
+++ resources/views/components/post-list.blade.php
@@ -0,0 +1,21 @@
+<div class="m-4 p-4">
+    <h1>나의 이름은 {{ $name }}입니다. </h1>
+    <table class="table table-hover">
+        <thead>
+            <th scope="col">제목</th>
+            <th scope="col">작성자</th>
+            <th scope="col">작성일</th>
+        </thead>
+        <tbody>
+            @foreach ($posts as $post)
+                <tr>
+                    <td><a href="{{ route('posts.show', ['post' => $post->id]) }}">{{ $post->content }}</a></td>
+                    <td>{{ $post->writer->name }}</td>
+                    <td>{{ $post->created_at->diffForHumans() }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+    {{ $posts->links() }}
+</div>
+</div>
