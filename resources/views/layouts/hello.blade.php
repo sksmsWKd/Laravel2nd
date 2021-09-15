ad338917dded99ed540ec27b47e3e064b7e186a8
Song Jae Hyeon <0nocsace@gmail.com>
2021-09-15 13:55:31 +0900

    blades

---
+++ resources/views/layouts/hello.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Hi~ everyone.. It's time to wrap up the class</h1>
+@endsection
+
+@section('content2')
+    <h2>content2입니다</h2>
+@endsection
