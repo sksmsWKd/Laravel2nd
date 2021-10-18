5e620eb0fc228e45c326791604d47d6d071fe68a
Song Jae Hyeon <0nocsace@gmail.com>
2021-10-18 20:32:08 +0900

    comment eloquent

---
+++ app/Models/Comment.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['comment' => 'required|max:100',];
+
+    public function user()
+    {
+        //comment 입장에서 연결된 
+        //belongsTo 관계메서드를 통해 연결시키면 ㅇ
+
+        return $this->belongsTo(User::class);
+        /*
+            select * from users
+            where id = $this.user.id
+            만약에 관례안따르면 pk줘야함
+        */
+    }
+}
